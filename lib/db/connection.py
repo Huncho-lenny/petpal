441271915882f7ce25b2842d8cabf22b1a97da59
lenny <lenny.kimanthi@student.moringaschool.com>
2025-05-29 19:42:15 +0300

    testownerandpet

--- lib/db/connection.py
+++ lib/db/connection.py
@@ -1,4 +1,27 @@
 import sqlite3
 
-CONN = sqlite3.connect('petpal.db')
-CURSOR = CONN.cursor()
+connection = sqlite3.connect('petpal.db')
+cursor = connection.cursor()
+
+cursor.execute("DROP TABLE IF EXISTS owners;")
+cursor.execute("DROP TABLE IF EXISTS pets;")
+
+cursor.execute("""
+CREATE TABLE owners (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+);
+""")
+
+cursor.execute("""
+CREATE TABLE pets (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    pet_type TEXT NOT NULL,
+    owner_id INTEGER,
+    FOREIGN KEY(owner_id) REFERENCES owners(id)
+);
+""")
+
+connection.commit()
+connection.close()
--- schema.sql
+++ schema.sql
@@ -2,14 +2,14 @@ DROP TABLE IF EXISTS owners;
 DROP TABLE IF EXISTS pets;
 
 CREATE TABLE owners (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL
+    id INTEGER PRIMARY KEY,
+    name TEXT
 );
 
 CREATE TABLE pets (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    pet_type TEXT NOT NULL,
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    pet_type TEXT,
     owner_id INTEGER,
-    FOREIGN KEY(owner_id) REFERENCES owners(id)
+    FOREIGN KEY (owner_id) REFERENCES owners(id)
 );
